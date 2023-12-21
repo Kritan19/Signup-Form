58a29a65fae7abb22ba3183fe27fe1f9cd172789
kritan <nepalkritan47@gmail.com>
2023-12-21 20:28:50 +0545

    validation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const button = document.getElementById("button")
+const input = document.getElementsByTagName('input')
 function submit(){
     alert("Your form has been submitted!")
+    input.innerText+='';
 }
